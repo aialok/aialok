9ac242b905df47535ed7dfc46cfee5eeae2b476d
Alok Gupta <alokgupta1560@gmail.com>
2024-06-18 17:38:26 +0530

    updated portfolio

---
+++ README.md
@@ -0,0 +1,40 @@
+# Hi there! 👋 I'm Alok Gupta
+
+## About Me
+I'm Alok Gupta, a sophomore at IIIT Ranchi, and a passionate open-source enthusiast. I love to gossip about Node.js and thrive on creating big, super-smooth backends and APIs.
+
+![Profile Views](https://komarev.com/ghpvc/?username=aialok&label=Profile%20views&color=0e75b6&style=flat)
+
+
+### Tech Stack
+I have hands-on experience with various technologies including:
+- JavaScript | TypeScript
+- React | Redux | Tailwind
+- Node.js | Express.js | Socket.io
+- MongoDB | Mongoose (ODM)
+- MySQL | Sequelize (ORM)
+- Jest (for unit tests)
+- Docker | Postman | Linux
+
+### 🚀 Currently Learning
+I'm currently diving deep into the world of GraphQL, exploring its capabilities, and integrating it into my tech stack.
+
+### 💡 Techy Quote
+> "The strength of JavaScript is that you can do anything. The weakness is that you will." - Reg Braithwaite
+
+### 🌐 Let's Connect
+- [LinkedIn](https://www.linkedin.com/in/aialok/)
+- [Twitter](https://twitter.com/ai_alok)
+
+### 📊 GitHub Stats
+![Top Languages](https://github-readme-stats.vercel.app/api/top-langs/?username=aialok&layout=compact)
+
+![GitHub Stats](https://github-readme-stats-peach-pi.vercel.app/api?username=aialok&show_icons=true&hide_title=true&include_all_commits=true&count_private=true&bg_color=45,2b8eaf,b222a8&text_color=ffffff&icon_color=ffffff&title_color=ffffff&border_color=000000)
+
+### 🚀 GitHub Streak
+![GitHub Streak](https://github-readme-streak-stats.herokuapp.com/?user=aialok)
+
+
+
+[![An image of @aialok's Holopin badges, which is a link to view their full Holopin profile](https://holopin.me/aialok)](https://holopin.io/@aialok)
+
---
+++ app/page.tsx
@@ -0,0 +1,77 @@
+import Link from "next/link";
+import Image from "next/image";
+import hackathon from "../public/hackathon.jpg";
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="font-medium text-2xl mb-8 tracking-tighter">hey, I am Alok 👊</h1>
+      <p className="prose prose-neutral dark:prose-invert">
+        I am open source contributor and developer. I am passionate about building. I am always curious about how things really work.
+        I currently working as Google Summer of code contributor at <Link href="https://www.postman.com/">Postman</Link>. I am also a part of <Link href="https://www.json-schema.org/">JSON Schema</Link> organization as contributor and maintainer of projects. 
+        </p>
+        <div className="grid grid-cols-2 grid-rows-4 sm:grid-rows-3 sm:grid-cols-3 gap-4 my-8">
+        <div className="relative h-40">
+          <Image
+            alt="Me speaking on stage at React Summit about the future of Next.js"
+            src={hackathon}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative sm:row-span-2 row-span-1">
+          <Image
+            alt="Me standing on stage at Reactathon delivering the keynote"
+            src={hackathon}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover object-top sm:object-center"
+          />
+        </div>
+        <div className="relative">
+          <Image
+            alt="Me and Guillermo Rauch on stage for Vercel Ship, answering questions from the Next.js community"
+            src={hackathon}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative row-span-2">
+          <Image
+            alt="Me, Lydia, and Delba filming the Next.js Conf keynote"
+            src={hackathon}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover sm:object-center"
+          />
+        </div>
+        <div className="relative row-span-2">
+          <Image
+            alt="My badge on top of a pile of badges from a Vercel meetup we held"
+            src={hackathon}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+        <div className="relative h-40">
+          <Image
+            alt="Me standing on stage at SmashingConf giving a talk about my optimism for the web"
+            src={hackathon}
+            fill
+            sizes="(max-width: 768px) 213px, 33vw"
+            priority
+            className="rounded-lg object-cover"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
